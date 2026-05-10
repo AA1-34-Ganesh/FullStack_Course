831c1f05d870a064601f227885e241fe2fbc74ce
AA1-34-Ganesh <guntaganesh1@gmail.com>
2026-05-10 10:35:18 +0530

    Complete 1.3 to 1.5 excersises in fullstackopen

--- part1/courseinfo/src/components/Total.jsx
+++ part1/courseinfo/src/components/Total.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
 
 const Total = (props) => {
+     const total = props.parts[0].exercises + props.parts[1].exercises +props.parts[2].exercises;
   return (
     <div>
-      Total Number of exercises in a part1: {props.total}
+       Number of exercises: {total}
     </div>
   )
 }
