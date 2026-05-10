e2c79336478e019c19a745c77eddd00d2dc3bbd2
AA1-34-Ganesh <guntaganesh1@gmail.com>
2026-05-10 09:33:04 +0530

    Complete 1.1 and 1.2 exercise in full stack open course

---
+++ part1/courseinfo/src/components/Total.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Total = (props) => {
+  return (
+    <div>
+      Total Number of exercises in a part1: {props.total}
+    </div>
+  )
+}
+
+export default Total
