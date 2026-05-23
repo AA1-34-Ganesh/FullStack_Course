0bfd7c99dd2a347d4911841e59f034e44eac0ecf
AA1-34-Ganesh <guntaganesh1@gmail.com>
2026-05-23 12:41:55 +0530

    Add the button to show details of specific country in 2.19 exercise

--- part2/countries/src/App.jsx
+++ part2/countries/src/App.jsx
@@ -1,43 +1,22 @@
 import React, { useEffect } from "react";
 import { useState } from "react";
 import axios from 'axios';
+import Button from "./Button";
 
 
 function App(){
   const [search,setSearch]=useState('');
   const [displayData,setDisplayData]=useState([]);
- 
+  const [selectedCountry,setSelectedCountry]=useState(null);
   const handleInputChange=(event)=>{
     setSearch(event.target.value);
+    setSelectedCountry(null);
   }
   const filterData=displayData.filter((country)=> 
     country.name.common.toLowerCase().includes(search.toLowerCase())
   )
-  useEffect(()=>{
- axios.get("https://studies.cs.helsinki.fi/restcountries/api/all").then((response)=>{
-    setDisplayData(response.data);
-  })
-  },[])
-  return(
-      <div>
-
-        <form>
-          Find Countries  <input value={search} onChange={handleInputChange} />
-        
-        </form>
-    
-        <div>
-        {filterData.length > 10 ? (
-          <p>Too many matches, specify another filter.</p>
-        ) : filterData.length > 1 ? (
-          filterData.map((country) => (
-            <div key={country.cca3}>
-              {country.name.common}
-            </div>
-          ))
-        ) : (
-          filterData.map((country) => (
-            <div key={country.cca3}>
+  const  renderSpecificCountryDetails=(country)=>(
+             <div key={country.cca3}>
               <h2>{country.name.common}</h2>
 
               <p>
@@ -66,8 +45,40 @@ function App(){
                 width="200"
               />
             </div>
+  )
+  useEffect(()=>{
+ axios.get("https://studies.cs.helsinki.fi/restcountries/api/all").then((response)=>{
+    setDisplayData(response.data);
+  })
+  },[])
+  return(
+      <div>
+
+        <form>
+          Find Countries  <input value={search} onChange={handleInputChange} />
+        
+        </form>
+    
+        <div>
+        {
+         filterData.length > 10 ? (
+          <p>Too many matches, specify another filter.</p>
+        ) : filterData.length > 1 ? (
+          filterData.map((country) => (
+            <div key={country.cca3}>
+              {country.name.common}
+              <Button text= "show" onClick={()=>setSelectedCountry(country)}/>
+                {
+                  (selectedCountry?.cca3 === country?.cca3 && renderSpecificCountryDetails(country))
+                }
+            </div>
           ))
-        )}
+        ) : filterData.length===1?(
+          renderSpecificCountryDetails(filterData[0])
+        ):(
+           <p>No countries found</p>
+        )
+      }
       </div>
      
       </div>
