ca69a617564d4b719cb567bd1ebb5bf41d0ef8cc
AA1-34-Ganesh <guntaganesh1@gmail.com>
2026-06-03 19:23:09 +0530

    Add mongodb connection and complete 3.12 exercise

--- part2/phonebook/backend/index.js
+++ part2/phonebook/backend/index.js
@@ -3,6 +3,7 @@ const morgan=require('morgan');
 const cors=require('cors');
 const path=require('path');
 const app = express();
+
 app.use(express.json());
 morgan.token('body',(request)=>{
     return JSON.stringify(request.body);
