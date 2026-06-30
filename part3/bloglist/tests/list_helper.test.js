ed7e9c473c6be2a08d8c2b8c8a1cac18dbc27213
AA1-34-Ganesh <guntaganesh1@gmail.com>
2026-06-30 18:41:29 +0530

    Complete 4.5 exercise and add favouriteBlog function

---
+++ part3/bloglist/tests/list_helper.test.js
@@ -0,0 +1,76 @@
+const { test, describe } = require('node:test')
+const assert = require('node:assert')
+const listHelper = require('../utils/list_helper')
+
+test('dummy returns one', () => {
+  const blogs = []
+
+  const result = listHelper.dummy(blogs)
+  assert.strictEqual(result, 1)
+})
+
+describe('total likes', () => {
+
+  test('empty list returns 0', () => {
+    assert.strictEqual(listHelper.totalLikes([]), 0)
+  })
+
+  test('single blog', () => {
+    assert.strictEqual(
+      listHelper.totalLikes([
+        { likes: 5 }
+      ]),
+      5
+    )
+  })
+
+})
+
+describe('favorite blog', () => {
+
+  test('empty list returns null', () => {
+    const result = listHelper.favoriteBlog([])
+
+    assert.strictEqual(result, null)
+  })
+
+  test('single blog is favorite', () => {
+    const blogs = [
+      {
+        title: 'Node.js',
+        author: 'Ganesh',
+        likes: 5
+      }
+    ]
+
+    const result = listHelper.favoriteBlog(blogs)
+
+    assert.deepStrictEqual(result, blogs[0])
+  })
+
+  test('returns blog with most likes', () => {
+    const blogs = [
+      {
+        title: 'React',
+        author: 'A',
+        likes: 7
+      },
+      {
+        title: 'MongoDB',
+        author: 'B',
+        likes: 20
+      },
+      {
+        title: 'Express',
+        author: 'C',
+        likes: 12
+      }
+    ]
+
+    const result = listHelper.favoriteBlog(blogs)
+
+    assert.deepStrictEqual(result, blogs[1])
+  })
+
+})
+
--- part3/bloglist/utils/list_helper.js
+++ part3/bloglist/utils/list_helper.js
@@ -4,7 +4,16 @@ const dummy = (blogs) => {
 const totalLikes = (blogs) => {
   return blogs.reduce((sum, blog) => sum + blog.likes, 0)
 }
+const favoriteBlog = (blogs) => {
+  if (blogs.length === 0) {
+    return null
+  }
+  return blogs.reduce((favorite, current) => {
+    return current.likes > favorite.likes ? current : favorite
+  })
+}
 module.exports = {
   dummy,
-  totalLikes
+  totalLikes,
+  favoriteBlog
 }
