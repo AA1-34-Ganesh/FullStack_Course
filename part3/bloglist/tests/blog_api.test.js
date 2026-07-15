5e82ba55eff643a63c3fe50f1e88f0e96e8c9026
AA1-34-Ganesh <guntaganesh1@gmail.com>
2026-07-15 17:05:02 +0530

    Complete 4.11 exercise

--- part3/bloglist/tests/blog_api.test.js
+++ part3/bloglist/tests/blog_api.test.js
@@ -80,6 +80,22 @@ test('creates a new blog post',async()=>{
 
   assert(titles.includes(newBlog.title))
 })
+
+test('if likes property is missing, it defaults to 0', async () => {
+  const newBlog = {
+    title: 'JavaScript',
+    author: 'Ganesh',
+    url: 'javascript.com'
+  }
+
+  const response = await api
+    .post('/api/blogs')
+    .send(newBlog)
+    .expect(201)
+    .expect('Content-Type', /application\/json/)
+
+  assert.strictEqual(response.body.likes, 0)
+})
 after(async () => {
   await mongoose.connection.close()
 })
